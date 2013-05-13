b12fd2d54fdd986357f21b167ed73da78858ea6a
ShinichiIshizuka <ip_shin@yahoo.co.jp>
2013-05-14 00:23:12 +0900

    ファイル追加

---
+++ Project/Src/Ong.Forms.StandardControls/Inside/FormsControlBase.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Codeer.Friendly.Windows.Grasp;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly;
+using Ong.Friendly.FormsStandardControls.Inside;
+
+namespace Ong.Friendly.FormsStandardControls
+{
+    /// <summary>
+    /// FormsControlBase
+    /// </summary>
+    public class FormsControlBase : WindowControl
+    {
+        /// <summary>
+        /// コンストラクタです
+        /// </summary>
+        /// <param name="src">元となるウィンドウコントロールです</param>
+        public FormsControlBase(WindowControl src)
+            : base(src)
+        {
+            Initializer.Initialize(App, GetType());
+        }
+
+        /// <summary>
+ 　     /// コンストラクタです
+        /// </summary>
+        /// <param name="app">アプリケーション操作クラス</param>
+        /// <param name="appVar">アプリケーション内変数</param>
+        public FormsControlBase(WindowsAppFriend app, AppVar appVar)
+            : base(app, appVar)
+        {
+            Initializer.Initialize(app, GetType());
+        }
+        
+        /// <summary>
+        /// テキストを取得します
+        /// </summary>
+        /// <returns>テキスト</returns>
+        public String Text
+        {
+            get { return (String)this["Text"]().Core; }
+        }
+    }
+}
