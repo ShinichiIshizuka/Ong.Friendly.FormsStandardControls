ba0ac01e181e4d1c814b71391c17dc1cb12e01b3
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2013-05-15 21:27:14 +0900

    ファイル追加

---
+++ Project/Src/Ong.Forms.StandardControls/FormsToolStripTextBox.cs
@@ -0,0 +1,29 @@
+﻿using Codeer.Friendly;
+using Codeer.Friendly.Windows;
+
+namespace Ong.Friendly.FormsStandardControls
+{
+    /// <summary>
+    /// ツールストリップアイテム操作クラス
+    /// </summary>
+    public class FormsToolStripTextBox : FormsToolStripItem
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="app">対象アプリ操作クラス</param>
+        /// <param name="appVar">対象アプリケーション内変数操作クラス</param>
+        public FormsToolStripTextBox(WindowsAppFriend app, AppVar appVar) : base(app, appVar) { }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="item">ToolStripItem操作クラス</param>
+        public FormsToolStripTextBox(FormsToolStripItem item) : base(item.App, item.AppVar) { }
+ 
+        /// <summary>
+        /// テキストボックス取得
+        /// </summary>
+        public FormsTextBox ComboBox { get { return new FormsTextBox(App, this["TextBox"]()); } }
+    }
+}
