aae01cd9e75b6ebc3be16cffc21cbaa589b82126
ShinichiIshizuka <ip_shin@yahoo.co.jp>
2013-05-22 19:30:50 +0900

    コミット漏れ

---
+++ Project/Src/Ong.Forms.StandardControls/FormsListViewSubItem.cs
@@ -0,0 +1,34 @@
+using System;
+using Codeer.Friendly;
+using Ong.Friendly.FormsStandardControls.Inside;
+using Codeer.Friendly.Windows;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Ong.Friendly.FormsStandardControls
+{
+    /// <summary>
+    /// リストビューサブアイテム
+    /// </summary>
+    public class FormsListViewSubItem:AppVarWrapper
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="app">アプリケーション操作クラス</param>
+        /// <param name="appVar">アプリケーション内変数</param>
+        public FormsListViewSubItem(WindowsAppFriend app, AppVar appVar)
+            : base(app, appVar)
+        {
+        }
+    
+        /// <summary>
+        /// テキストを取得します
+        /// </summary>
+        /// <returns>テキスト</returns>
+        public String Text
+        {
+            get { return (String)this["Text"]().Core; }
+        }
+    }
+}
