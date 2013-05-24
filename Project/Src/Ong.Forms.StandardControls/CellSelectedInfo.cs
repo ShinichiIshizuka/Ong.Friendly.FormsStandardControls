93b568f170ab6004a10ce15d768025208f787eb1
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2013-05-24 23:03:47 +0900

    コメントに。を付ける

--- Project/Src/Ong.Forms.StandardControls/CellSelectedInfo.cs
+++ Project/Src/Ong.Forms.StandardControls/CellSelectedInfo.cs
@@ -3,7 +3,7 @@
 namespace Ong.Friendly.FormsStandardControls
 {
     /// <summary>
-    /// セル選択情報
+    /// セル選択情報。
     /// </summary>
     [Serializable]
     public class CellSelectedInfo
@@ -13,26 +13,26 @@ namespace Ong.Friendly.FormsStandardControls
         bool _selected;
 
         /// <summary>
-        /// 列
+        /// 列。
         /// </summary>
         public int Col { get { return _col; } set { _col = value; } }
 
         /// <summary>
-        /// 行
+        /// 行。
         /// </summary>
         public int Row { get { return _row; } set { _row = value; } }
 
         /// <summary>
-        /// 選択状態であるか
+        /// 選択状態であるか。
         /// </summary>
         public bool Selected { get { return _selected; } set { _selected = value; } }
 
         /// <summary>
-        /// コンストラクタ
+        /// コンストラクタ。
         /// </summary>
-        /// <param name="col">行</param>
-        /// <param name="row">列</param>
-        /// <param name="selected">選択状態であるか</param>
+        /// <param name="col">行。</param>
+        /// <param name="row">列。</param>
+        /// <param name="selected">選択状態であるか。</param>
         public CellSelectedInfo(int col, int row, bool selected)
         {
             _col = col;
