fa73d511e80dda40d654dffab211b58f0de33282
ShinichiIshizuka <ip_shin@yahoo.co.jp>
2013-06-10 11:21:37 +0900

    テストメンテ

--- Project/Src/Test/ListBoxTest.cs
+++ Project/Src/Test/ListBoxTest.cs
@@ -3,6 +3,8 @@ using Codeer.Friendly.Windows;
 using Codeer.Friendly.Windows.Grasp;
 using Ong.Friendly.FormsStandardControls;
 using System.Diagnostics;
+using Codeer.Friendly;
+using System.Windows.Forms;
 namespace Test
 {
     /// <summary>
@@ -74,10 +76,38 @@ namespace Test
         {
             FormsListBox listbox2 = new FormsListBox(app, testDlg["listBox2"]());
             int[] select = new int[]{5};
-            listbox2.EmulateChangeSelectedIndex(5);
+            listbox2.EmulateChangeSelectedIndex(5,new Async());
             int selected = listbox2.SelectedIndex;
-            Assert.AreEqual(5, selected);
+            Assert.AreEqual(1, selected);
         }
 
+        /// <summary>
+        /// リストアイテムを選択状態にします。選択一覧を取得します。
+        /// </summary>
+        [Test]
+        public void TestSelectAndGetIndexes()
+        {
+            FormsListBox listbox2 = new FormsListBox(app, testDlg["listBox2"]());
+            int[] select = new int[] { 1, 2, 4 };
+            listbox2.EmulateChangeSelectedIndex(1, new Async());
+            listbox2.EmulateChangeSelectedIndex(2, new Async());
+            listbox2.EmulateChangeSelectedIndex(4, new Async());
+            int[] selected = listbox2.SelectedIndexes;
+            Assert.AreEqual(1, selected[0]);
+            Assert.AreEqual(2, selected[1]);
+            Assert.AreEqual(4, selected[2]);
+        }
+
+        /// <summary>
+        /// 選択モード
+        /// </summary>
+        [Test]
+        public void TestSelectionMode()
+        {
+            FormsListBox listbox1 = new FormsListBox(app, testDlg["listBox1"]());
+            FormsListBox listbox2 = new FormsListBox(app, testDlg["listBox2"]());
+            Assert.AreEqual(SelectionMode.One, listbox1.SelectionMode);
+            Assert.AreEqual(SelectionMode.MultiSimple, listbox2.SelectionMode);
+        }
     }
 }
