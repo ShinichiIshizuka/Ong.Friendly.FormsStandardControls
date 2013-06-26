d9a1b34d719c02be50c744e62d9e92a77514ad7e
ShinichiIshizuka <ip_shin@yahoo.co.jp>
2013-06-27 01:07:32 +0900

    TabControl @@@

--- Project/Src/Test/TabControlTest.cs
+++ Project/Src/Test/TabControlTest.cs
@@ -4,6 +4,9 @@ using Codeer.Friendly.Windows;
 using Codeer.Friendly.Windows.Grasp;
 using Ong.Friendly.FormsStandardControls;
 using System.Diagnostics;
+using System.Windows.Forms;
+using System;
+using Codeer.Friendly.Windows.NativeStandardControls;
 namespace Test
 {
     /// <summary>
@@ -24,6 +27,7 @@ namespace Test
             //テスト用の画面起動
             app = new WindowsAppFriend(Process.Start(Settings.TestApplicationPath), "2.0");
             testDlg = WindowControl.FromZTop(app);
+            WindowsAppExpander.LoadAssemblyFromFile(app, GetType().Assembly.Location);
         }
         
         /// <summary>
@@ -65,13 +69,37 @@ namespace Test
 
         /// <summary>
         /// EmulateTabSelect
-        /// @@@非同期
         /// </summary>
         [Test]
         public void TestTabSelect()
         {
             FormsTabControl tabcontrol1 = new FormsTabControl(app, testDlg["tabControl1"]());
-            tabcontrol1.EmulateTabSelect(2,new Async());
+            tabcontrol1.EmulateTabSelect(1);
+            Assert.AreEqual(1, tabcontrol1.SelectedIndex);
+
+            //非同期
+            app[GetType(), "TabSelectEvent"](tabcontrol1.AppVar);
+            tabcontrol1.EmulateTabSelect(2, new Async());
+            new NativeMessageBox(testDlg.WaitForNextModal()).EmulateButtonClick("OK");
+            Assert.AreEqual(2, tabcontrol1.SelectedIndex);
+        }
+
+        /// <summary>
+        /// 状態変更時にメッセージボックスを表示する
+        /// </summary>
+        /// <param name="tabcontrol">タブコントロール</param>
+        static void TabSelectEvent(TabControl tabcontrol)
+        {
+            EventHandler handler = null;
+            handler = delegate
+            {
+                MessageBox.Show("");
+                tabcontrol.BeginInvoke((MethodInvoker)delegate
+                {
+                    tabcontrol.SelectedIndexChanged -= handler;
+                });
+            };
+            tabcontrol.SelectedIndexChanged += handler;
         }
     }
 }
