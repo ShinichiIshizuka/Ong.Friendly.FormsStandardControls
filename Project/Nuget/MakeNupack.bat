b71fb3c9a7fc06be2ae7d6e0d2a934fb95254b1b
Tatsuya Ishikawa <stone.guitar.777@gmail.com>
2017-11-24 19:31:37 +0900

    version up.

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,4 +1,4 @@
 rd /s /q "../Src/ReleaseBinary"
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "../Src/Ong.Friendly.FormsStandardControls.sln" /rebuild Release
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "../Src/Ong.Friendly.FormsStandardControls.sln" /rebuild Release-Eng
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../Src/Ong.Friendly.FormsStandardControls.sln" /rebuild Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" "../Src/Ong.Friendly.FormsStandardControls.sln" /rebuild Release-Eng
 nuget pack friendly.formsstandardcontrols.nuspec
