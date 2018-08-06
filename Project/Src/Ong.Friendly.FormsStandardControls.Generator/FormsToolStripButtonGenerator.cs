f0c0413da4974c2136e7dc8be991b3d94c216c83
Tatsuya Ishikawa <ishikawa-tatsuya@codeer.co.jp>
2018-08-06 21:37:08 +0900

    Add ToolSripItemCaptorGenerator.

---
+++ Project/Src/Ong.Friendly.FormsStandardControls.Generator/FormsToolStripButtonGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using Codeer.TestAssistant.GeneratorToolKit;
+
+namespace Ong.Friendly.FormsStandardControls.Generator
+{
+#if ENG
+    /// <summary>
+    /// This class generates operation codes for FormsToolStripButton.
+    /// </summary>
+#else
+    /// <summary>
+    /// FormsToolStripButtonの操作コードを生成します。
+    /// </summary>
+#endif
+    [CaptureCodeGenerator("Ong.Friendly.FormsStandardControls.FormsToolStripButton")]
+    public class FormsToolStripButtonGenerator : CaptureCodeGeneratorBase
+    {
+        ToolStripButton _control;
+
+#if ENG
+        /// <summary>
+        /// Attach.
+        /// </summary>
+#else
+        /// <summary>
+        /// アタッチ。
+        /// </summary>
+#endif
+        protected override void Attach()
+        {
+            _control = (ToolStripButton)ControlObject;
+            _control.Click += ButtonClick;
+            _control.CheckedChanged += CheckedChanged;
+        }
+
+#if ENG
+        /// <summary>
+        /// Detach.
+        /// </summary>
+#else
+        /// <summary>
+        /// ディタッチ。
+        /// </summary>
+#endif
+        protected override void Detach()
+        {
+            if (_control == null) return;
+            _control.Click -= ButtonClick;
+            _control.Click -= CheckedChanged;
+        }
+
+        /// <summary>
+        /// ボタン押下
+        /// </summary>
+        /// <param name="sender">イベント送信元</param>
+        /// <param name="e">イベント内容</param>
+        void ButtonClick(object sender, EventArgs e)
+        {
+            AddSentence(new TokenName(), ".EmulateClick(", new TokenAsync(CommaType.Non), ");");
+        }
+
+        /// <summary>
+        /// チェック変更
+        /// </summary>
+        /// <param name="sender">イベント送信元</param>
+        /// <param name="e">イベント内容</param>
+        void CheckedChanged(object sender, EventArgs e)
+        {
+            AddSentence(new TokenName(), ".EmulateCheck(CheckState." + _control.CheckState, new TokenAsync(CommaType.Before), ");");
+        }
+
+    }
+}
---
+++ Project/Src/Ong.Friendly.FormsStandardControls.Generator/FormsToolStripItemGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+using Codeer.TestAssistant.GeneratorToolKit;
+
+namespace Ong.Friendly.FormsStandardControls.Generator
+{
+#if ENG
+    /// <summary>
+    /// This class generates operation codes for FormsToolStripItem.
+    /// </summary>
+#else
+    /// <summary>
+    /// FormsToolStripItemの操作コードを生成します。
+    /// </summary>
+#endif
+    [CaptureCodeGenerator("Ong.Friendly.FormsStandardControls.FormsToolStripItem")]
+    public class FormsToolStripItemGenerator : CaptureCodeGeneratorBase
+    {
+        ToolStripItem _control;
+
+#if ENG
+        /// <summary>
+        /// Attach.
+        /// </summary>
+#else
+        /// <summary>
+        /// アタッチ。
+        /// </summary>
+#endif
+        protected override void Attach()
+        {
+            _control = (ToolStripItem)ControlObject;
+            _control.Click += ButtonClick;
+        }
+
+#if ENG
+        /// <summary>
+        /// Detach.
+        /// </summary>
+#else
+        /// <summary>
+        /// ディタッチ。
+        /// </summary>
+#endif
+        protected override void Detach()
+        {
+            _control.Click -= ButtonClick;
+        }
+
+        /// <summary>
+        /// ボタン押下
+        /// </summary>
+        /// <param name="sender">イベント送信元</param>
+        /// <param name="e">イベント内容</param>
+        void ButtonClick(object sender, EventArgs e)
+        {
+            AddSentence(new TokenName(), ".EmulateClick(", new TokenAsync(CommaType.Non), ");");
+        }
+    }
+}
