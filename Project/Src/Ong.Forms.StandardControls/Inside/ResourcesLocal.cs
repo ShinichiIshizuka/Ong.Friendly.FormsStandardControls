cbbcec67ab8632d2861cca7ac87ac2a69bb554be
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2024-10-09 01:03:30 +0900

    Version up.

--- Project/Src/Ong.Forms.StandardControls/Inside/ResourcesLocal.cs
+++ Project/Src/Ong.Forms.StandardControls/Inside/ResourcesLocal.cs
@@ -8,14 +8,29 @@ namespace Ong.Friendly.FormsStandardControls.Inside
     /// ローカライズ済みリソース。
     /// </summary>
     [Serializable]
-    class ResourcesLocal
+    public class ResourcesLocal
     {
         static internal ResourcesLocal Instance;
 
-        internal string ErrorCheckSetting;
-        internal string ErrorNotSetListView;
-        internal string ErrorNotSetToolStrip;
-        internal string ErrorNotSetTreeView;
+        /// <summary>
+        /// ErrorCheckSetting
+        /// </summary>
+        public string ErrorCheckSetting { get; set; }
+
+        /// <summary>
+        /// ErrorNotSetListView
+        /// </summary>
+        public string ErrorNotSetListView { get; set; }
+
+        /// <summary>
+        /// ErrorNotSetToolStrip
+        /// </summary>
+        public string ErrorNotSetToolStrip { get; set; }
+
+        /// <summary>
+        /// ErrorNotSetTreeView
+        /// </summary>
+        public string ErrorNotSetTreeView { get; set; }
 
         /// <summary>
         /// 初期化。
@@ -29,7 +44,7 @@ namespace Ong.Friendly.FormsStandardControls.Inside
         }
 
         /// <summary>
-        /// コンストラクタ。
+        /// 初期化。
         /// </summary>
         void Initialize()
         {
