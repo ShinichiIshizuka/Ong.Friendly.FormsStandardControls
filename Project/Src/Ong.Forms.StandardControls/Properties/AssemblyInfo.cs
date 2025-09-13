859d64924cf5596562d75ce2a9792ea62e459f4c
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2025-09-13 17:07:38 +0900

    version up

--- Project/Src/Ong.Forms.StandardControls/Properties/AssemblyInfo.cs
+++ Project/Src/Ong.Forms.StandardControls/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // すべての値を指定するか、下のように '*' を使ってリビジョンおよびビルド番号を 
 // 既定値にすることができます:
-[assembly: AssemblyVersion("2.22.0.0")]
-[assembly: AssemblyFileVersion("2.22.0.0")]
+[assembly: AssemblyVersion("2.23.0.0")]
+[assembly: AssemblyFileVersion("2.23.0.0")]
 [assembly: System.CLSCompliant(true)]
